7af6d333fd56bb785c4b19290273a42c50034e3d
Ephraim Jan Conales <conalesej@gmail.com>
2023-09-07 18:48:11 +0800

    POST Requests

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,25 @@ const app = express();
 
 const PORT = 3001;
 
+app.use(express.json());
+app.use(express.urlencoded());
+
 app.listen(PORT, () => {
   console.log(`Running Express server on port ${PORT}`);
 });
 
-app.get("/posts", (request, response) => {
-  response.send({ Hello: "World" });
+const groceriesList = [
+  { item: "milk", quantity: 3 },
+  { item: "eggs", quantity: 6 },
+  { item: "pop-tarts", quantity: 9 },
+];
+
+app.get("/groceries", (request, response) => {
+  response.send({ status: 200, data: groceriesList });
+});
+ 
+app.post("/grocery", (request, response) => {
+  console.log(request.body);
+  groceriesList.push(request.body);
+  response.send({ status: 201 });
 });
