da3aa110841255456be4e62280b1c1889aeac34d
Ephraim Jan Conales <conalesej@gmail.com>
2023-09-07 17:40:24 +0800

    GET Requests

--- src/index.ts
+++ src/index.ts
@@ -7,4 +7,7 @@ const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Running Express server on port ${PORT}`);
 });
-console.log("Hello World");
+
+app.get("/posts", (request, response) => {
+  response.send({ Hello: "World" });
+});
