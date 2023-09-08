59d1dc46edb24fcd6b73b9141e6f57d5eef7f218
Ephraim Jan Conales <conalesej@gmail.com>
2023-09-08 21:49:57 +0800

    Middleware

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ const app = express();
 
 const PORT = 3001;
 
+// Make sure this is declared first before using any middle ware
 app.use(express.json());
 app.use(express.urlencoded());
 
@@ -17,12 +18,39 @@ const groceriesList = [
   { item: "pop-tarts", quantity: 9 },
 ];
 
-app.get("/groceries", (request, response) => {
-  response.send({ status: 200, data: groceriesList });
+// this will trigger as well
+app.use((req, res, next) => {
+  console.log(`${req.method}: ${req.url}`);
+  console.log("Middleware 1");
+  // Perform some actions or checks here
+  // If everything is okay, call next() to pass control to the next middleware
+  next();
 });
- 
+
+app.get(
+  "/groceries",
+  // Middleware function
+  (request, response, next) => {
+    console.log("Before Handling Request");
+    next();
+  },
+  // Middleware function
+  (request, response, next) => {
+    console.log("Redirecting request");
+    next();
+  },
+  (request, response, next) => {
+    // this can be the end
+    response.send({ status: 200, data: groceriesList });
+    next();
+  },
+  (request, response, next) => {
+    // this can be the end
+    console.log("Ending request");
+  }
+);
+
 app.post("/grocery", (request, response) => {
-  console.log(request.body);
   groceriesList.push(request.body);
   response.send({ status: 201 });
 });
