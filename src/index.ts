4fad0fc89cd78cc40f756834ac5aabfba185836c
Ephraim Jan Conales <conalesej@gmail.com>
2023-09-09 22:04:16 +0800

    added groceries router

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import express = require("express");
-
+import { router as groceriesRoute } from "./routes/groceries";
 const app = express();
 
 const PORT = 3001;
@@ -8,56 +8,9 @@ const PORT = 3001;
 app.use(express.json());
 app.use(express.urlencoded());
 
+// We can prefix this with "api" or anything you want
+app.use("/groceries", groceriesRoute);
+
 app.listen(PORT, () => {
   console.log(`Running Express server on port ${PORT}`);
-});
-
-const groceriesList = [
-  { item: "milk", quantity: 3 },
-  { item: "eggs", quantity: 6 },
-  { item: "pop-tarts", quantity: 9 },
-];
-
-// this will trigger as well
-app.use((req, res, next) => {
-  console.log(`${req.method}: ${req.url}`);
-  console.log("Middleware 1");
-  // Perform some actions or checks here
-  // If everything is okay, call next() to pass control to the next middleware
-  next();
-});
-
-app.get(
-  "/groceries",
-  // Middleware function
-  (request, response, next) => {
-    console.log("Before Handling Request");
-    next();
-  },
-  // Middleware function
-  (request, response, next) => {
-    console.log("Redirecting request");
-    next();
-  },
-  (request, response, next) => {
-    request.params;
-    response.send({ status: 200, data: groceriesList });
-    next();
-  },
-  (request, response, next) => {
-    // this can be the end
-    console.log("Ending request");
-  }
-);
-
-// GET http://localhost:3001/groceries
-app.get("/groceries/:item/", (req, res) => {
-  const { item } = req.params;
-  const itemObject = groceriesList.find((it) => it.item === item);
-  res.send(itemObject);
-});
-
-app.post("/grocery", (request, response) => {
-  groceriesList.push(request.body);
-  response.send({ status: 201 });
 });
