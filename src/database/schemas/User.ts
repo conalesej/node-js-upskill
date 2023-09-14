fe14d455e7c7dc32183bbef62917a71ea05038d2
Ephraim Jan Conales <conalesej@gmail.com>
2023-09-15 00:13:12 +0800

    add mongoose as database

---
+++ src/database/schemas/User.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: mongoose.SchemaTypes.String,
+    required: true,
+  },
+  password: {
+    type: mongoose.SchemaTypes.String,
+    required: true,
+  },
+  email: {
+    type: mongoose.SchemaTypes.String,
+    required: true,
+  },
+  createdAt: {
+    type: mongoose.SchemaTypes.Date,
+    required: true,
+    default: new Date(),
+  },
+});
+
+const UserModel = mongoose.model("users", UserSchema);
+
+export default UserModel;
